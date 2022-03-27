76d540b9becfe69a1843b16d017ad77738700e94
Lucas19Almeida <almeidalucas788@gmail.com>
2022-03-27 18:23:09 -0300

    27/03 - Ajuste na IDE

--- src/academy/devdojo/maratonajava/introducao/OlaMundo.java
+++ src/academy/devdojo/maratonajava/introducao/OlaMundo.java
@@ -2,6 +2,6 @@ package academy.devdojo.maratonajava.introducao;
 
 public class OlaMundo {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo");
+        System.out.println("Olá Mundo!");
     }
 }
